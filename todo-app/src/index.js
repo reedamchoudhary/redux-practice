69588070f4cef3eb0074cd73d045c690924c0eed
Reedam <reedamchoudhary169@gmail.com>
2021-05-28 10:22:35 +0530

    adding delete functionality

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -8,7 +8,7 @@ import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider>
+  <Provider store={store}>
     <App />
     </Provider>
   </React.StrictMode>,
--- todo-app/src/reducers/todos.js
+++ todo-app/src/reducers/todos.js
@@ -2,8 +2,25 @@ const initialState = {
     data: [],
 };
 
-const todos = (state = initialState, actions)=>{
-    switch(actions.type){
+const todos = (state = initialState, action)=>{
+    switch(action.type){
+        case 'ADD_TODO':
+            return {
+                ...state,
+                data:[
+                    ...state.data,
+                    {
+                        message:action.message,
+                        id:action.id,
+                    }
+                ]
+            };
+        case 'DELETE_TODO':
+            return {};
+        default :
+        return state;
         
     }
-}
+};
+
+export default todos
